31f38a84817dbda77f69321b80cca4dbd4c910d1
Haulund <steffen.haulund@gmail.com>
2018-05-25 10:59:38 +0200

    frokost commit steffen

--- index.php
+++ index.php
@@ -26,35 +26,104 @@
 </article>
 
 <article class="grid-container">
+        <div class="item0"><h3>Hvad kan jeg hjælpe med</h3></div>
         <div class="item1">
                 <a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a>
-                <a href="#">borgerservice</a>
-                <a href="#">Pas</a>
-                <a href="#">NemID</a>
-                <a href="">Meld at du flytter</a>
-                <a href="#">Kørekort</a>
-                <a href="#">Sundhedskort</a>
+                <a href="#"><h4>Borgerservice</h4></a>
+                <a href="#">Pas,</a>
+                <a href="#">NemID,</a>
+                <a href="#">Meld at du flytter,</a>
+                <a href="#">Kørekort,</a>
+                <a href="#">Sundhedskort,</a>
                 <a href="#">Se alle</a>
         </div>
-        <div class="item2"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#" >Børn, unge og familie</a></div>
-        <div class="item3"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#" >Ældre</a></div>
-        <div class="item4"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#">Miljø & Teknik</a></div>
-        <div class="item5"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#">Kultur & Fritid</a></div>
-        <div class="item6"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#">Bolig & Byggeri</a></div>
-        <div class="item7"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#">Sundhed & Sygdom</a></div>
-        <div class="item8"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#">Personlig hjælp & støtte</a></div>
-        <div class="item9"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#">Job & Uddannelse</a></div>
+        <div class="item2">
+                <a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a>
+                <a href="#"><h4>Børn, unge og familie</h4></a>
+                <a href="#">Dagtilbud,</a>
+                <a href="#">Orlov & Barse,l</a>
+                <a href="#">Familieydelser,</a>
+                <a href="#">Parforhold & Ægteskab,</a>
+                <a href="#">Se alle</a>
+        </div>
+        <div class="item3">
+                <a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri">
+                </a><a href="#" ><h4>Ældre</h4></a>
+                <a href="#">Dagjem,</a>
+                <a href="#">Demenstilbud,</a>
+                <a href="#">Folkepension,</a>
+                <a href="#">Rådgivning</a>
+                <a href="#">Se alle</a>
+        </div>
+        <div class="item4">
+                <a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a>
+                <a href="#"><h4>Miljø & Teknik</h4></a>
+                <a href="#">Affald & genbrug,</a>
+                <a href="#">Rottebekæmpelse,</a>
+                <a href="#">Parkering,</a>
+                <a href="#">Veje & Stier</a>
+                <a href="#">Se alle</a>
+        </div>
+        <div class="item5">
+                <a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a>
+                <a href="#"><h4>Kultur & Fritid</h4></a>
+                <a href="#">Idræt,</a>
+                <a href="#">Oplevelser,</a>
+                <a href="#">Aftenskoler,</a>
+                <a href="#">Foreninger,</a>
+                <a href="#">Lokaler,</a>
+                <a href="#">Se alle</a>
+        </div>
+        <div class="item6">
+                <a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a>
+                <a href="#"><h4>Bolig & Byggeri</h4></a>
+                <a href="#">Lokalplaner,</a>
+                <a href="#">Boliger,</a>
+                <a href="#">Byggetilladselser,</a>
+                <a href="#">Bredbåndsdækning,</a>
+                <a href="#">Skorstensfejning,</a>
+                <a href="#">Se alle</a>
+        </div>
+        <div class="item7">
+                <a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a>
+                <a href="#"><h4>Sundhed & Sygdom</h4></a>
+                <a href="#">Misbrug,</a>
+                <a href="#">Sundhedstilbud,</a>
+                <a href="#">Pårørerende,</a>
+                <a href="#">Begravelseshjælp,</a>
+                <a href="#">Afhængighed,</a>
+                <a href="#">Se alle</a>
+        </div>
+        <div class="item8">
+                <a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a>
+                <a href="#"><h4>Personlig hjælp & støtte</h4></a>
+                <a href="#">Handicap,</a>
+                <a href="#">Familiestøtte,</a>
+                <a href="#">Vold,</a>
+                <a href="#">Rådgivning,</a>
+                <a href="#">Se alle</a>
+        </div>
+        <div class="item9">
+                <a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a>
+                <a href="#"><h4>Job & Uddannelse</h4></a>
+                <a href="#">Ledig,</a>
+                <a href="#">SU,</a>
+                <a href="#">Skoler,</a>
+                <a href="#">Økonomi,</a>
+                <a href="#">Nedsat arbejdsevne,</a>
+                <a href="#">Se alle</a>
+        </div>
 </article>
 
 <div>
 
 </div>
 
-<a href="#">Pas</a>
-<a href="#">NemID</a>
-<a href="">Meld at du flytter</a>
-<a href="#">Kørekort</a>
-<a href="#">Sundhedskort</a>
+<a href="#">Ledig,</a>
+<a href="#">SU,</a>
+<a href="#">Skoler,</a>
+<a href="#">Økonomi,</a>
+<a href="#">Nedsat arbejdsevne,</a>
 <a href="#">Se alle</a>
 
 <?php include 'footer.php';?>
