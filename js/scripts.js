3562cbce4b939dc9750f325ca2c316714788e920
Kasper Rasmussen <kasper@Kaspers-MacBook-Pro-2.local>
2018-05-25 09:57:29 +0200

    Slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready( function() {
-    
+   
+
+	var bannerslider = $('.slideshow').bxSlider({
+		slideMargin: 5,
+		pager: true,
+		controls: false,
+		auto: true,
+		pause: '5500',
+		speed: '300',
+		useCSS: false
+	});
+
 });
