4f1a446ff09a143ea55dba192f7dc36d1dffe634
Haulund <steffen.haulund@gmail.com>
2018-05-25 10:27:19 +0200

    Merge

--- index.php
+++ index.php
@@ -26,20 +26,35 @@
 </article>
 
 <article class="grid-container">
-        <div class="item1"><a href="#">borgerservice</a></div>
-        <div class="item2"><a href="#" >Børn, unge og familie</a></div>
-        <div class="item3"><a href="#" >Ældre</a></div>
-        <div class="item4"><a href="#">Miljø & Teknik</a></div>
-        <div class="item5"><a href="#">Kultur & Fritid</a></div>
-        <div class="item6"><a href="#">Bolig & Byggeri</a></div>
-        <div class="item7"><a href="#">Sundhed & Sygdom</a></div>
-        <div class="item8"><a href="#">Personlig hjælp & støtte</a></div>
-        <div class="item9"><a href="#">Job & Uddannelse</a></div>
+        <div class="item1">
+                <a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a>
+                <a href="#">borgerservice</a>
+                <a href="#">Pas</a>
+                <a href="#">NemID</a>
+                <a href="">Meld at du flytter</a>
+                <a href="#">Kørekort</a>
+                <a href="#">Sundhedskort</a>
+                <a href="#">Se alle</a>
+        </div>
+        <div class="item2"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#" >Børn, unge og familie</a></div>
+        <div class="item3"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#" >Ældre</a></div>
+        <div class="item4"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#">Miljø & Teknik</a></div>
+        <div class="item5"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#">Kultur & Fritid</a></div>
+        <div class="item6"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#">Bolig & Byggeri</a></div>
+        <div class="item7"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#">Sundhed & Sygdom</a></div>
+        <div class="item8"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#">Personlig hjælp & støtte</a></div>
+        <div class="item9"><a href="#"><img src="img/bolig-byggeri.png" alt="icon for bolig og byggeri"></a><a href="#">Job & Uddannelse</a></div>
 </article>
 
 <div>
 
 </div>
 
+<a href="#">Pas</a>
+<a href="#">NemID</a>
+<a href="">Meld at du flytter</a>
+<a href="#">Kørekort</a>
+<a href="#">Sundhedskort</a>
+<a href="#">Se alle</a>
 
 <?php include 'footer.php';?>
