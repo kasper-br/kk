26d01782b854075142a53d5177d61612ea103d3b
Kasper Rasmussen <kasper@Kaspers-MacBook-Pro-2.local>
2018-05-25 09:01:35 +0200

    Changes

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <?php include 'header.php';?>
 
 <article>
-asdasd
+    <ul class="slideshow">
+        <li>
+            <figure>
+                <figcaption>
+                    <h2>Vi holder ekstra åbnet til bestilling af pas</h2>
+                    <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod.</p>
+                    <a class="btn outline blue" href="#">Bestil pas her</a>
+                </figcaption>
+                <img src="img/pas-bestilling.png" alt="Bestil tid til dit pas i dag">
+            </figure>
+        </li>
+        <li>
+            <figure>
+                <figcaption>
+                    <h2>Vi holder ekstra åbnet til bestilling af pas</h2>
+                    <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod.</p>
+                    <a class="btn outline blue" href="#">Bestil pas her</a>
+                </figcaption>
+                <img src="img/flytning.png" alt="Husk at flytte din adresse">
+            </figure>
+        </li>
+    </ul>
 </article>
 
 
