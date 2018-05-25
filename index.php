bac5d254ba8ede0b5bc46704ae9cc766c73505e5
Haulund <steffen.haulund@gmail.com>
2018-05-25 09:59:58 +0200

    Test

--- index.php
+++ index.php
@@ -1,29 +1,24 @@
 <?php include 'header.php';?>
 
 <article>
-    <ul class="slideshow">
-        <li>
-            <figure>
-                <figcaption>
-                    <h2>Vi holder ekstra åbnet til bestilling af pas</h2>
-                    <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod.</p>
-                    <a class="btn outline blue" href="#">Bestil pas her</a>
-                </figcaption>
-                <img src="img/pas-bestilling.png" alt="Bestil tid til dit pas i dag">
-            </figure>
-        </li>
-        <li>
-            <figure>
-                <figcaption>
-                    <h2>Vi holder ekstra åbnet til bestilling af pas</h2>
-                    <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod.</p>
-                    <a class="btn outline blue" href="#">Bestil pas her</a>
-                </figcaption>
-                <img src="img/flytning.png" alt="Husk at flytte din adresse">
-            </figure>
-        </li>
-    </ul>
+asdasd
 </article>
+
+<article class="grid-container">
+        <div class="item1"><a href="#">borgerservice</a></div>
+        <div class="item2"><a href="#" >Børn, unge og familie</a></div>
+        <div class="item3"><a href="#" >Ældre</a></div>
+        <div class="item4"><a href="#">Miljø & Teknik</a></div>
+        <div class="item5"><a href="#">Kultur & Fritid</a></div>
+        <div class="item6"><a href="#">Bolig & Byggeri</a></div>
+        <div class="item7"><a href="#">Sundhed & Sygdom</a></div>
+        <div class="item8"><a href="#">Personlig hjælp & støtte</a></div>
+        <div class="item9"><a href="#">Job & Uddannelse</a></div>
+</article>
+
+<div>
+
+</div>
 
 
 <?php include 'footer.php';?>
