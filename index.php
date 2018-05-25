683a146afdb65602ba5f939281cb87519d710b94
Kasper Rasmussen <kasper@Kaspers-MacBook-Pro-2.local>
2018-05-25 10:59:37 +0200

    frokost

--- index.php
+++ index.php
@@ -4,22 +4,22 @@
     <ul class="slideshow">
         <li>
             <figure>
+                <img src="img/pas-bestilling.png" alt="Bestil tid til dit pas i dag">
                 <figcaption>
                     <h2>Vi holder ekstra åbnet til bestilling af pas</h2>
                     <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod.</p>
                     <a class="btn outline blue" href="#">Bestil pas her</a>
                 </figcaption>
-                <img src="img/pas-bestilling.png" alt="Bestil tid til dit pas i dag">
             </figure>
         </li>
         <li>
             <figure>
+                <img src="img/flytning.png" alt="Husk at flytte din adresse">
                 <figcaption>
                     <h2>Vidste du at du kan melde flytning online?</h2>
                     <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Vestibulum id ligula porta felis euismod semper. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod.</p>
                     <a class="btn outline blue" href="#">Bestil pas her</a>
                 </figcaption>
-                <img src="img/flytning.png" alt="Husk at flytte din adresse">
             </figure>
         </li>
     </ul>
