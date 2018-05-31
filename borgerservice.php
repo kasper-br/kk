77f6f5cd5178a32868b2af3fbec9dcb37f07944f
Haulund <steffen.haulund@gmail.com>
2018-05-31 14:00:00 +0200

    slut commit

---
+++ borgerservice.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Pas | Køge Kommune</title>
+    <meta name='description' content="Koege passport subpage. Made as an school assignment zealand institute of business and technology">
+
+
+<?php include 'header.php';?>
+
+<figure class="topimage borgerservice"></figure>
+
+<main>
+        <article class="borgergridwrapper">
+                <div class="borgergridcontainer">
+                        <div class="itemX">
+                                <a href="<?php echo $pasurl; ?>"> <h2>Pas</h2> </a>
+                                <p>Bestil pas eller læs mere om reglerne for pas</p>
+                        </div>
+
+                        <div class="itemX">
+                                <h2>Kørekort</h2>
+                                <p>Skal du have nyt kørekort, skal du henvende dig personligt i et af Borgerservicecentrene. Vær opmærksom på krav til fotos på kørekort. </p>
+                        </div>
+                        <div class="itemX">
+                                <h2>NemID</h2>
+                                <p>NemID er din adgangskode på internettet, som gør det sikkert for dig at se og oplyse fortrolige og følsomme oplysninger via internettet.</p>
+                        </div>
+                        <div class="itemX">
+                                <h2>Folkeregister</h2>
+                                <p>Senest fem dage efter, du er flyttet til Københavns Kommune eller til en anden adresse indenfor kommunen, skal du gå på nettet og melde flytning.</p>
+                        </div>
+                        <div class="itemX">
+                                <h2>Sygesikring</h2>
+                                <p>Sundhedskortet er navnet for det gule sygesikringsbevis, og det giver dig ret til sundhedsydelser.</p>
+                        </div>
+                        <div class="itemX">
+                                <h2>Pensionist</h2>
+                                <p>Læs om tilbud og aktiviteter, plejeboliger (plejehjem), hjælp og pleje, demens, aktiv aldring og meget mere.</p>
+                        </div>
+                        <div class="itemX">
+                                <h2>Helbredstillæg</h2>
+                                <p>Du kan søge om helbredstillæg, hvis du er pensionist og har helbredsudgifter</p>
+                        </div>
+                        <div class="itemX">
+                                <h2>Økonomi</h2>
+                                <p>Her er også informationer om en række forhold, der vedrører din økonomi såsom barselsdagpenge, SKAT, dine feriepenge, eller hvis du har gæld til kommunen.</p>
+                        </div>
+                        <div class="itemX">
+                                <h2>Rådgivning</h2>
+                                <p>Har du brug for hjælp eller personlig rådgivning</p>
+                        </div>
+                </div>
+        </article>
+
+</main>
+
+<?php include 'footer.php';?>
