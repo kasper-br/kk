76026c0399d34fe612fb0d5f5322b29d1b81612f
Haulund <steffen.haulund@gmail.com>
2018-06-01 09:56:41 +0200

    commit 2

--- borgerservice.php
+++ borgerservice.php
@@ -9,7 +9,7 @@
 
 <figure class="topimage borgerservice"></figure>
 
-<main>
+
         <article class="borgergridwrapper">
                 <div class="borgergridcontainer">
                         <div class="item0">
@@ -23,48 +23,48 @@
                         </div>
 
                         <div class="itemX">
-                                <h3><a href="#">Kørekort</a></h3>
+                                <a href="#"><h3>Kørekort</h3></a>
                                 <p>Skal du have nyt kørekort, skal du henvende dig personligt i et af Borgerservicecentrene. Vær opmærksom på krav til fotos på kørekort. </p>
                                 <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h3><a href="#">NemID</a></h3>
+                                <a href="#"><h3>NemID</h3></a>
                                 <p>NemID er din adgangskode på internettet, som gør det sikkert for dig at se og oplyse fortrolige og følsomme oplysninger via internettet.</p>
                                 <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h3><a href="#">Folkeregister</a></h3>
+                                <a href="#"><h3>Folkeregister</h3></a>
                                 <p>Senest fem dage efter, du er flyttet til Københavns Kommune eller til en anden adresse indenfor kommunen, skal du gå på nettet og melde flytning.</p>
                                 <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h3><a href="#">Sygesikring</a></h3>
+                                <a href="#"><h3>Sygesikring</h3></a>
                                 <p>Sundhedskortet er navnet for det gule sygesikringsbevis, og det giver dig ret til sundhedsydelser.</p>
                                 <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h3><a href="#">Pensionis</a></h3>
+                                <a href="#"><h3>Pensionist</h3></a>
                                 <p>Læs om tilbud og aktiviteter, plejeboliger (plejehjem), hjælp og pleje, demens, aktiv aldring og meget mere.</p>
                                 <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h3><a href="#">Helbredstillæg</a></h3>
+                                <a href="#"><h3>Helbredstillæg</h3></a>
                                 <p>Du kan søge om helbredstillæg, hvis du er pensionist og har helbredsudgifter</p>
                                 <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h3><a href="#">Økonomi</a></h3>
+                                <a href="#"><h3>Økonomi</h3></a>
                                 <p>Her er også informationer om en række forhold, der vedrører din økonomi såsom barselsdagpenge, SKAT, dine feriepenge, eller hvis du har gæld til kommunen.</p>
                                 <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h3><a href="#">Rådgivning</a></h3>
+                                <a href="#"><h3>Rådgivning</h3></a>
                                 <p>Har du brug for hjælp eller personlig rådgivning</p>
                                 <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                 </div>
         </article>
 
-</main>
+
 
 <?php include 'footer.php';?>
