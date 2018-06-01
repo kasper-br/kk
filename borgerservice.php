b7b373adbacd8bf33d790139554ac3f7648c3e87
Haulund <steffen.haulund@gmail.com>
2018-06-01 09:27:56 +0200

    commit

--- borgerservice.php
+++ borgerservice.php
@@ -12,42 +12,55 @@
 <main>
         <article class="borgergridwrapper">
                 <div class="borgergridcontainer">
-                        <div class="itemX">
-                                <a href="<?php echo $pasurl; ?>"> <h2>Pas</h2> </a>
-                                <p>Bestil pas eller læs mere om reglerne for pas</p>
+                        <div class="item0">
+                                <h2>Borgerservice</h2>
                         </div>
 
                         <div class="itemX">
-                                <h2>Kørekort</h2>
+                                <a href="<?php echo $pasurl; ?>"> <h3>Pas</h3> </a>
+                                <p>Bestil pas eller læs mere om reglerne for pas</p>
+                                <a href="#" class="btn blue outline">læs mere om pas</a>
+                        </div>
+
+                        <div class="itemX">
+                                <h3><a href="#">Kørekort</a></h3>
                                 <p>Skal du have nyt kørekort, skal du henvende dig personligt i et af Borgerservicecentrene. Vær opmærksom på krav til fotos på kørekort. </p>
+                                <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h2>NemID</h2>
+                                <h3><a href="#">NemID</a></h3>
                                 <p>NemID er din adgangskode på internettet, som gør det sikkert for dig at se og oplyse fortrolige og følsomme oplysninger via internettet.</p>
+                                <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h2>Folkeregister</h2>
+                                <h3><a href="#">Folkeregister</a></h3>
                                 <p>Senest fem dage efter, du er flyttet til Københavns Kommune eller til en anden adresse indenfor kommunen, skal du gå på nettet og melde flytning.</p>
+                                <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h2>Sygesikring</h2>
+                                <h3><a href="#">Sygesikring</a></h3>
                                 <p>Sundhedskortet er navnet for det gule sygesikringsbevis, og det giver dig ret til sundhedsydelser.</p>
+                                <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h2>Pensionist</h2>
+                                <h3><a href="#">Pensionis</a></h3>
                                 <p>Læs om tilbud og aktiviteter, plejeboliger (plejehjem), hjælp og pleje, demens, aktiv aldring og meget mere.</p>
+                                <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h2>Helbredstillæg</h2>
+                                <h3><a href="#">Helbredstillæg</a></h3>
                                 <p>Du kan søge om helbredstillæg, hvis du er pensionist og har helbredsudgifter</p>
+                                <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h2>Økonomi</h2>
+                                <h3><a href="#">Økonomi</a></h3>
                                 <p>Her er også informationer om en række forhold, der vedrører din økonomi såsom barselsdagpenge, SKAT, dine feriepenge, eller hvis du har gæld til kommunen.</p>
+                                <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                         <div class="itemX">
-                                <h2>Rådgivning</h2>
+                                <h3><a href="#">Rådgivning</a></h3>
                                 <p>Har du brug for hjælp eller personlig rådgivning</p>
+                                <a href="#" class="btn blue outline">læs mere om pas</a>
                         </div>
                 </div>
         </article>
